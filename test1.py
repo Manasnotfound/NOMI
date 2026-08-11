92cab84beb73983231d4a8a1d850dfa015b39563
Manas Mishra <manasmishra98935@gmail.com>
2026-08-12 03:18:19 +0530

    TEST DAY2

--- test1.py
+++ test1.py
@@ -1,24 +1,23 @@
 from email.mime import text
-import os
+from pdb import main
 import speech_recognition as sr
-import time
-import cv2
+import pyttsx3
 
-def speech_to_text():
+""" def speech_to_text():
     with sr.Microphone() as source:
         print("Listening...")
         recognizer = sr.Recognizer()
         audio = recognizer.listen(source)   
         text = recognizer.recognize_google(audio)
-        print(text)
-    speech_to_text()
- 
-def input_command():
-    command = input("Press Enter to ask NOMI a question...").casefold()
-    if command == "nomi capture":
-        print("NOMI Capture command received, capturing image...")
-        return True
-    return False
+        return text """
 
+def command_input():
+    text = input("Enter your command: ")
+    pyttsx3.speak(f"the following command has been received: {text}")
+    if "Nomi" in text:
+        print("Nomi is awake")
+    text = text.split()
+    print(text)
+                
 if __name__ == "__main__":
-    speech_to_text()
+    command_input()
